cdd5d3b7276acefb81bd29e41ef54581d9e53bac
Bhavnoor Saroya <bsaroya3@my.bcit.ca>
2023-03-05 20:26:18 -0800

    looking real good
    I have multiple assignments due but I'm working on this :)

--- scripts/inject.js
+++ scripts/inject.js
@@ -7,3 +7,10 @@ function injectTemplates() {
 
 // Calls the inject_navfoot function
 injectTemplates();
+
+
+//this lil guy makes the bouncy arrow fade out when you scroll down
+$(window).scroll(function(){
+    $("#bouncyarrow").css("opacity", 1 - $(window).scrollTop() / 400); 
+  //250 is fade pixels
+  });
