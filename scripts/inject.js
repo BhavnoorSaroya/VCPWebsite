eba2367ca13859d532c1133f731413b78577c07b
Bhavnoor Saroya <bsaroya3@my.bcit.ca>
2023-03-06 00:08:11 -0800

    added animations on scroll I am so happy its official I made a website with animations on scroll

--- scripts/inject.js
+++ scripts/inject.js
@@ -12,5 +12,17 @@ injectTemplates();
 //this lil guy makes the bouncy arrow fade out when you scroll down
 $(window).scroll(function(){
     $("#bouncyarrow").css("opacity", 1 - $(window).scrollTop() / 400); 
-  //250 is fade pixels
+    if ($(window).scrollTop() == 0) {
+        $('.arrow').slideDown(600);
+    }
+    else if($(window).scrollTop() > 400){
+        $('.arrow').slideUp(600);
+    }
   });
+
+
+
+
+function showhide() {
+    $('.arrow').slideToggle(200);
+}
